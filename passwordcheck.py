485de5b5385bde56c93299996b0d4b3dcfc887e6
B RAM SAI TEJA <saitejain8055@gmail.com>
2024-12-22 00:13:03 +0530

    updated-2

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,5 +1,7 @@
 import requests
 import hashlib
+import sys
+
 def req_api_data(query_char):
     res = requests.get(f'https://api.pwnedpasswords.com/range/{query_char}')
     if res.status_code != 200:
@@ -9,21 +11,29 @@ def req_api_data(query_char):
 def passwords_leaks(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
 
-
+def main(args):
+    for password in args:
+        count = pewend_api_check(password)
+        if count:
+            print(f'{password} was found in {count} times.... you should')
+        else:
+            print(f'{password} was NOT FOUND. carry on!')
+    return 'done'
 
 def pewend_api_check(password):
     m = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    frist5_char = m[:5] 
-    tail = m[5:]
+    frist5_char , tail= m[:5], m[5:]
     response = req_api_data(frist5_char)
     print(response)
-    return passwords_leaks(frist5_char, tail)
+    return passwords_leaks(response, tail)
 
-# print(req_api_data('21BD1'))
 
-pewend_api_check('123')
+
+main(sys.argv[1:])
 
 
 
